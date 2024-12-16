4d859080118a4986014e8519c2da3e616c752f5a
mikeian <zhouwenxiang@ciwei.net>
2024-12-16 18:55:50 +0800

    test groovy base syntax language

--- src/main/groovy/Main.groovy
+++ src/main/groovy/Main.groovy
@@ -1,3 +1,24 @@
+/**
+ * this is a simple comment
+ * @param args
+ */
 static void main(String[] args) {
-  println "Hello world!"
+  // test comment
+  println "Hello' \"world!" // end comment
+
+  def number = 3.14
+
+  def sParameterLessClosure = """1 + 2 == ${-> 
+    println "nested expression ... "
+    return 3
+  }"""
+
+  println sParameterLessClosure
+
+  def sOneParamClosure = """1 + 2 == ${ w ->
+    println "w is ${w}"  
+    w << 3
+  }"""
+
+  println sOneParamClosure
 }

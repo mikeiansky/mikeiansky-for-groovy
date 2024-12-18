2b252788f04b92b142eac0960231e0c442cd56c5
mikeian <zhouwenxiang@ciwei.net>
2024-12-18 18:48:39 +0800

    test config

--- src/main/groovy/Main.groovy
+++ src/main/groovy/Main.groovy
@@ -2,23 +2,8 @@
  * this is a simple comment
  * @param args
  */
+import static java.util.Calendar.getInstance as now
+
 static void main(String[] args) {
-  // test comment
-  println "Hello' \"world!" // end comment
-
-  def number = 3.14
-
-  def sParameterLessClosure = """1 + 2 == ${-> 
-    println "nested expression ... "
-    return 3
-  }"""
-
-  println sParameterLessClosure
-
-  def sOneParamClosure = """1 + 2 == ${ w ->
-    println "w is ${w}"  
-    w << 3
-  }"""
-
-  println sOneParamClosure
+  println now()
 }

6626603fc2657cd568f9004da9ae880817754f9c
mikeian <zhouwenxiang@ciwei.net>
2025-01-17 12:12:33 +0800

    update code

--- src/main/groovy/Main.groovy
+++ src/main/groovy/Main.groovy
@@ -2,10 +2,44 @@
  * this is a simple comment
  * @param args
  */
-import static java.util.Calendar.getInstance as now
+
+class Dog {
+  void test(){
+    def hello = {
+      println("dog hello this ${this}")
+    }
+    hello()
+  }
+
+  class Inner {
+    def ts = {println("inner this is ${this}")}
+  }
+
+  def createInner(){
+    return new Inner()
+  }
+
+}
 
 static void main(String[] args) {
-  def isOdd = { int i -> i%2 != 0 }
-  println(isOdd(2))
-  println(isOdd(3))
+
+//  println("main this ${this}")
+////  println("main getThisObject ${getThisObject()}")
+//
+//  def testThis = {
+//    println("test this ${this}")
+//    println("test getThisObject ${getThisObject()}")
+//  }
+//
+//  testThis()
+
+
+  def dog = new Dog()
+  println("dog $dog")
+
+  dog.test()
+
+  def di = dog.createInner()
+  di.ts()
+
 }
